7cf1868a9ac5de516a76295579e8a550e471e4a4
yhexie <yhlx125@163.com>
2017-05-17 11:27:29 +0800

    实现点云保存，坐标系之间的关系需要再考虑一下！
    Signed-off-by: yhexie <yhlx125@163.com>

--- KinectThird/KinectThird/KinectThird.cpp
+++ KinectThird/KinectThird/KinectThird.cpp
@@ -28,6 +28,12 @@ inline void SafeRelease(Interface *& pInterfaceToRelease)
 	}
 }
 
+const double camera_factor = 1000;
+const double camera_cx = 263.73696;
+const double camera_cy = 201.72450;
+const double camera_fx = 379.40726;
+const double camera_fy = 378.54472;
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//相机内参
@@ -36,8 +42,8 @@ int _tmain(int argc, _TCHAR* argv[])
 				 0, 1087.37528, 530.35240,
 				 0, 0, 1;
 	Eigen::Matrix3d intricDepth;
-	intricDepth << 379.40726, 0, 263.73696,
-		           0, 378.54472, 201.72450,
+	intricDepth << camera_fx, 0, camera_cx,
+				   0, camera_fy, camera_cy,
 		           0, 0, 1;
 	Eigen::Matrix3d intricdepth2RGB;
 	intricdepth2RGB = intricRGB*intricDepth.inverse();
@@ -78,7 +84,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	IColorFrame* m_pColorFrame = NULL;
 	// 三个图片格式
 	Mat i_rgb(1080, 1920, CV_8UC4);      //注意：这里必须为4通道的图，Kinect的数据只能以Bgra格式传出
-	Mat i_depth(424, 512, CV_8UC1);
+	Mat i_depth(424, 512, CV_16UC1);
 	Mat i_ir(424, 512, CV_16UC1);
 	Mat result(424, 512, CV_8UC4);
 
@@ -120,8 +126,8 @@ int _tmain(int argc, _TCHAR* argv[])
 			for (int i = 0; i < 512 * 424; i++)
 			{
 				// 0-255深度图，为了显示明显，只取深度数据的低8位
-				BYTE intensity = static_cast<BYTE>(depthData[i] % 256);
-				reinterpret_cast<BYTE*>(i_depth.data)[i] = intensity;
+				UINT16 intensity = static_cast<UINT16>(depthData[i]);
+				reinterpret_cast<UINT16*>(i_depth.data)[i] = intensity;
 			}
 
 			// 实际是16位unsigned int数据
@@ -140,10 +146,10 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 			for (int col = 0; col < 512; col++)
 			{
-				BYTE* p = (BYTE*)i_depth.data;
-				BYTE depthValue = static_cast<BYTE>(p[row * 512 + col]);
+				UINT16* p = (UINT16*)i_depth.data;
+				UINT16 depthValue = static_cast<UINT16>(p[row * 512 + col]);
 				//cout << "depthValue       " << depthValue << endl;
-				if (depthValue != -std::numeric_limits<BYTE>::infinity() && depthValue != -std::numeric_limits<BYTE>::infinity() && depthValue != 0 && depthValue != 65535)
+				if (depthValue != -std::numeric_limits<UINT16>::infinity() && depthValue != -std::numeric_limits<UINT16>::infinity() && depthValue != 0 && depthValue != 65535)
 				{
 					// 投影到彩色图上的坐标
 					Eigen::Vector3d uv_depth(col, row, 1.0f);
@@ -159,18 +165,16 @@ int _tmain(int argc, _TCHAR* argv[])
 						result.data[i * 4 + 1] = i_rgb.data[4 * (Y * 1920 + X) + 1];
 						result.data[i * 4 + 2] = i_rgb.data[4 * (Y * 1920 + X) + 2];
 						result.data[i * 4 + 3] = i_rgb.data[4 * (Y * 1920 + X) + 3];
+						
 					}
 				}
 				i++;
 			}
 		}
-
-
-
 		// 显示
-		imshow("rgb", i_rgb);
-		if (waitKey(1) == VK_ESCAPE)
-			break;
+		//imshow("rgb", i_rgb);
+		//if (waitKey(1) == VK_ESCAPE)
+		//	break;
 		imshow("depth", i_depth);
 		if (waitKey(1) == VK_ESCAPE)
 			break;
@@ -180,7 +184,50 @@ int _tmain(int argc, _TCHAR* argv[])
 		/*imshow("ir", i_ir);
 		if (waitKey(1) == VK_ESCAPE)
 			break;*/
+		if (waitKey(100) == VK_SPACE)
+		{
+			SYSTEMTIME st;
+			GetLocalTime(&st);
+			char output_file[32];
+			char output_RGB[32];
+			char output_depth[32];
+			char output_ir[32];
+			sprintf_s(output_file, "%4d-%2d-%2d-%2d-%2d-%2d.txt", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+			sprintf_s(output_RGB, "%4d-%2d-%2d-%2d-%2d-%2d-rgb.png", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+			sprintf_s(output_depth, "%4d-%2d-%2d-%2d-%2d-%2d-depth.png", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+			sprintf_s(output_ir, "%4d-%2d-%2d-%2d-%2d-%2d-ir.png", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+			FILE *file = fopen(output_file, "w");
+			int idx = 0;
+			for (int row = 0; row < 424; row++)
+			{
+				for (int col = 0; col < 512; col++)
+				{
+					UINT16* p = (UINT16*)i_depth.data;
+					UINT16 depthValue = static_cast<UINT16>(p[row * 512 + col]);
+					
+					if (depthValue != -std::numeric_limits<UINT16>::infinity() && depthValue != 0)
+					{
+						double z = double(depthValue) / camera_factor;
+						double x = (col - camera_cx) * z / camera_fx;
+						double y = (row - camera_cy) * z / camera_fy;
 
+  						float cameraX = -static_cast<float>(x);
+						float cameraY = static_cast<float>(z);
+						float cameraZ = -static_cast<float>(y);
+						if (file)
+						{
+							int b = result.data[idx * 4 + 0];
+							int g = result.data[idx * 4 + 1];
+							int r = result.data[idx * 4 + 2];
+   							fprintf(file, "%.4f %.4f %.4f %d %d %d\n", cameraX, cameraY, cameraZ, r, g, b);
+						}
+					}
+					idx++;
+				}
+			}
+			fclose(file);
+			cout << "文件保存成功" << endl;
+		}
 		// 释放资源
 		SafeRelease(m_pColorFrame);
 		SafeRelease(m_pDepthFrame);
